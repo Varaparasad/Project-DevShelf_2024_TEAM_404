182c2403a1bb94d4b635add6f0f66843bb50f36c
Varaparasad <karyadavaraprasad@gmail.com>
2024-07-08 00:21:56 +0530

    Create userschema.js

---
+++ models/userschema.js
@@ -0,0 +1,39 @@
+const mongooose=require('mongoose')
+const { stringify } = require('querystring')
+
+const subsc1 = new mongooose.Schema({
+    title: String,
+    description: String,
+    author: String,
+    genre: String,
+    department: String,
+    vendor: String,
+    vendor_id: Number,
+    publisher: String,
+    publisher_id: Number,
+    date:String,
+    returndate:String
+})
+const subsc2 = new mongooose.Schema({
+    title: String,
+    description: String,
+    author: String,
+    genre: String,
+    department: String,
+    vendor: String,
+    vendor_id: Number,
+    publisher: String,
+    publisher_id: Number
+})
+
+
+let sc=new mongooose.Schema({
+    name:String,
+    password:String,
+    email:String,
+    phonenumber:Number,
+    mybooks:[subsc1],
+    mywishlist:[subsc2]
+})
+
+module.exports= new mongooose.model('userdetails',sc)
